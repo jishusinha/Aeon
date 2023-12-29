f0029d2ff2c64f013cae2c1df57283ccfc893fd1
Jishu Developer <143621473+jishusinha@users.noreply.github.com>
2023-12-29 18:55:37 +0530

    Update status.py

--- bot/modules/status.py
+++ bot/modules/status.py
@@ -48,5 +48,5 @@ async def status_pages(_, query):
         await turn_page(data)
 
 
-bot.add_handler(MessageHandler(mirror_status, filters=command(BotCommands.StatusCommand) & CustomFilters.authorized))
+bot.add_handler(MessageHandler(mirror_status, filters=command(BotCommands.StatusCommand)))
 bot.add_handler(CallbackQueryHandler(status_pages, filters=regex("^status")))
